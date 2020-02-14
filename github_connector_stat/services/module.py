467ce8d88e667a62e5e6f250ba8355301f7679ac
Sébastien Beau <sebastien.beau@akretion.com>
2020-02-14 20:12:55 +0100

    wip

--- github_connector_stat/services/module.py
+++ github_connector_stat/services/module.py
@@ -9,7 +9,7 @@ import logging
 from odoo.exceptions import AccessError
 from odoo.tools.translate import _
 
-from odoo.addons.base_rest.components.service import to_bool
+from odoo.addons.base_rest.components.service import skip_secure_params
 from odoo.addons.component.core import Component
 
 _logger = logging.getLogger(__name__)
@@ -21,6 +21,31 @@ class GithubModuleService(Component):
     _collection = "github.module"
     _usage = "module"
 
-    def get_github_module_information(self):
+#    def _validator_return_github_module_information(self):
+#        return {
+ #       }
+
+    @skip_secure_params
+    def github_module_information(self):
+#        sync_fields = [
+#            'name',
+#            'technical_name',
+#            'description_rst',
+#            'description_rst_html',
+#            'author_ids_description',
+#            'organization_serie_ids',
+#        ]
+#        data = self.env["odoo.module"].search_read([], sync_fields)
+        data = []
         modules = self.env["odoo.module"].search([])
-        return
+        for module in modules:
+            data.append({
+                'name': module.name,
+                'technical_name': module.technical_name,
+                'description_rst': module.description_rst,
+                'description_rst_html': module.description_rst_html,
+                'author_ids_description': module.author_ids_description,
+                # remove duplicates
+	        'versions': list(set(module.organization_serie_ids.mapped('name'))),
+            })
+        return data
